52bbe5d15a4ad0982e8f7c77ca50c3b2205793fd
chrisribia <31062468+chrisribia@users.noreply.github.com>
2020-08-18 11:26:13 +0300

    User login,Signup and Logout

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\User;
+use\Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+   public function Login(Request $request){
+    $request->validate([
+        'email' => 'required|string|email',
+        'password' => 'required|string']);
+   
+   $credentials = request(['email','password']);
+   if(!Auth::attempt($credentials)){
+return response()->json(['message' => 'Invalid email or Password'],401);
+
+
+   }
+
+   $user = $request->user();
+   $token = $user->createToken('Access Token');
+
+   $user->access_token = $token->accessToken;
+   return response()->json([
+       "user"=>$user
+   ],200);
+   
+   
+    }
+
+   
+   public function SignUp(Request $request){
+
+    $request->validate([
+       'name' => 'required|string',
+       'email' => 'required|string|email|unique:users',
+       'password' => 'required|string|confirmed']);
+
+$user = new User([
+'name' => $request->name,
+'email' => $request->email,
+'password'=>bcrypt($request->password)
+]);
+
+$user->save();
+return response()->json([
+    "message"  => "User registered successfully"
+],201);
+
+     
+}
+
+
+public function logout(Request $request){
+
+    $request->user()->token()->revoke();
+    return response()->json([
+        "message" => "user logged out successfully"
+    ],200);
+}
+public function Index(){
+    echo "index endpoint Requested";
+}
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::namespace('Api')->group(function(){
+Route::prefix('auth')->group(function(){
+    Route::post('login','AuthController@login');
+    Route::post('SignUp','AuthController@SignUp');
+
+});
+
+Route::group([
+    'middleware'=>'auth:api'
+], function(){
+    Route::get('helloWorld','AuthController@Index');
+    Route::post('logout','AuthController@logout');
+});
+
 });
